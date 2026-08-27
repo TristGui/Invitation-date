9d6bc673b20d33cdbb103b184cfa962d5649ae49
Tristan Guiraudou <trg9638@gmail.com>
2026-08-27 22:09:14 +0200

    ajout de restaurant

--- components/places-map.tsx
+++ components/places-map.tsx
@@ -17,8 +17,9 @@ const customIcon = new L.Icon({
 
 // Vos adresses de restaurants à Paris / IDF
 const restaurants = [
-  { id: 1, name: "Mon Resto Préféré", lat: 48.8833, lng: 2.3785, note: "Top pour un d\u00eener en amoureux !" },
-  { id: 2, name: "Brunch du Dimanche", lat: 48.8566, lng: 2.3522, note: "Pancakes incroyables" },
+  { id: 1, name: "PNY Marais", lat: 48.85819138153039, lng: 2.3570741064783562, note: "Top pour un burger en amoureux !" },
+  { id: 2, name: "Brunch du Dimanche", lat: 48.864025780701134, lng: 2.4332550097704697, note: "Top pour un étoilé en amoureux !" },
+  { id: 3, name: "Brunch du Dimanche", lat: 48.8737686194688, lng: 2.3629348418253646, note: "Top pour des nouilles en amoureux !" },
 ]
 
 // Vos destinations de voyage
