94946991daf4493ca5ee27ab0aa422eb3d31544a
Tristan Guiraudou <trg9638@gmail.com>
2026-08-28 20:21:03 +0200

    Ajout de lieux

--- components/places-map.tsx
+++ components/places-map.tsx
@@ -20,16 +20,18 @@ const restaurants = [
   { id: 1, name: "PNY Marais", lat: 48.85819138153039, lng: 2.3570741064783562, note: "Top pour un burger en amoureux !" },
   { id: 2, name: "La villa 9trois", lat: 48.864025780701134, lng: 2.4332550097704697, note: "Top pour un étoilé en amoureux !" },
   { id: 3, name: "La taverne de Zaho", lat: 48.8737686194688, lng: 2.3629348418253646, note: "Top pour des nouilles en amoureux !" },
-  { id: 4, name: "Les Gros Tonton de Paname", lat: 48.868538570156744, lng: 2.3543216199354737, note: "Top pour des gros steaks en amoureux !" }
+  { id: 4, name: "Les Gros Tonton de Paname", lat: 48.868538570156744, lng: 2.3543216199354737, note: "Top pour des gros steaks en amoureux !" },
+  { id: 5, name: "Les enfants Perdus", lat: 48.874348956258174, lng: 2.361880744594249, note: "Top pour des gros plats français en amoureux !" }
 ]
 // Vos destinations de voyage
 const trips = [
   { id: 1, place: "Marrakech", lat: 31.6295, lng: -7.9811, desc: "Voyage en mars 2026 \ud83c\udf34" },
-  { id: 2, place: "Guatemala", lat: 14.6349, lng: -90.5069, desc: "Aventure en ao\u00fbt 2026 \ud83c\udf0b" },
+  { id: 2, place: "Guatemala", lat: 14.6349, lng: -90.5069, desc: "Voyage en ao\u00fbt 2026 \ud83c\udf0b" },
   { id: 3, place: "Venise", lat: 45.440363109186606, lng: 12.340798236436102, desc: "Voyage en mars 2024 \ud83c\udf0b" },
-  { id: 5, place: "Chypre", lat: 35.042583179702405, lng: 33.237540196903296, desc: "Voyage en mai 2025 \ud83c\udf0b" },
+  { id: 5, place: "Chypre", lat: 35.042583179702405, lng: 33.237540196903296, desc: "Voyage en mai 2024 \ud83c\udf0b" },
   { id: 6, place: "Corfou", lat: 39.62537403113246, lng: 19.846881451067688, desc: "Voyage en ao\u00fbt 2025 \ud83c\udf0b" },
-  { id: 7, place: "Palawan", lat: 9.60251531893106, lng: 118.53339712993507, desc: "Voyage en novembre 2023 \ud83c\udf0b" }
+  { id: 7, place: "Palawan", lat: 9.60251531893106, lng: 118.53339712993507, desc: "Voyage en novembre 2023 \ud83c\udf0b" },
+  { id: 8, place: "Lac de Côme", lat: 46.02131602239564, lng: 9.260044831435698, desc: "Voyage en mai 2025 \ud83c\udf0b" }
 ]
 
 export default function PlacesMap() {
